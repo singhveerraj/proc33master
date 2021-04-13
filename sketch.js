15526feb7ef9bf1e37d741022e22c19c458231d9
singhveerraj <72805099+singhveerraj@users.noreply.github.com>
2021-04-13 12:25:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,116 @@
+const Engine = Matter.Engine;
+  const World = Matter.World;
+ const Events = Matter.Events;
+ const Bodies = Matter.Bodies;
+ var divisions = [];
+var particle = [];
+var particle;
+var plinkos = [];
+var count =0;
+var gameState = "start";
+
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ text("Score : "+score,20,30);
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+    divisions[k].display();
+  }
+   if(particle!=null){
+     particle.display();
+
+     if(particle.body.position.y>760){
+       if(particle.body.position.x<300){
+         score = score+500;
+         particle=null;
+         if(count>=5){
+           gameState = "end";
+         }
+       }
+       else if(particle.body.position.x>301 && particle.body.position.x<600){
+        score = score+100;
+        particle=null;
+        if(count>=5){
+          gameState = "end";
+        }
+       }
+else if(particle.body.position.x>601 && particle.body.position.x<900){
+  score = score+200;
+  particle=null;
+  if(count>=5){
+    gameState = "end";
+  }
+     
+   }
+  }
+}
+
+
+ 
+  
+}
+
+function keyPressed (){
+  if(keyCode === 32){
+
+ 
+  if (gameState!=="end"){
+    count++;
+    particle = new Particle(mouseX,10,10);
+  }
+}
+}
